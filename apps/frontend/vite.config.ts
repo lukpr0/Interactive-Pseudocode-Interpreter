14fac3aa9054552013109afc50c40a56e86d85b4
Lukas Huber <Lukas.Huber@Siemens.com>
2025-11-06 12:11:17 +0100

    configure svelte to output single file

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { viteSingleFile } from 'vite-plugin-singlefile'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), viteSingleFile()]
+
+	build: {
+		assetsInlineLimit: Infinity
+	},
+
+	plugins: [sveltekit()]
 });
