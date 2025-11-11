58f355b9ce4183bf0edc5c628b43881a982f333e
Lukas Huber <lukas.hu.02@gmail.com>
2025-11-11 14:20:42 +0100

    made option component

---
+++ apps/frontend/src/lib/Option.svelte
@@ -0,0 +1,19 @@
+
+<div class="option">
+    <input id={name} {name} type="checkbox" bind:checked={ checked }>
+    <label for={name}>{@render children()}</label>
+</div>
+
+<script>
+    let {
+        children,
+        name = "",
+        checked = $bindable(false)
+    } = $props()
+</script>
+
+<style>
+    .option {
+        display: flex;
+    }
+</style>
