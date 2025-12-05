8ecfe758d4bbb53b8b58753d5bbfa6048d9ceb25
Lukas Huber <Lukas.Huber@Siemens.com>
2025-12-05 14:59:03 +0100

    fix markup generation

---
+++ apps/frontend/src/lib/markupGenerationVisitor.ts
@@ -0,0 +1,64 @@
+import { BinaryOperationTree, type ArrayTree, type AssignTree, type BreakTree, type ContinueTree, type DotAccessorTree, type ExprTree, type ForTree, type FullIdTree, type FunctionCallTree, type FunctionTree, type IfTree, type IndexAccessorTree, type IteratorTree, type KeyValueTree, type ObjectTree, type ProgramTree, type RangeTree, type RepeatUntilTree, type ReturnTree, type StatListTree, type UnaryOperationTree, type Visitor, type WhileTree } from "@interactive-pseudo/interpreter";
+import { PseudoLexer } from "@interactive-pseudo/parser";
+
+export abstract class MarkupGenerationVisitor implements Visitor<string> {
+
+    protected headers: boolean
+
+    constructor(headers: boolean) {
+        this.headers = headers;
+    }
+
+    abstract visitProgram(program: ProgramTree): string; 
+    abstract visitAssign(assign: AssignTree): string; 
+    abstract visitExpr(expr: ExprTree): string; 
+    abstract visitBinary(expr: BinaryOperationTree): string; 
+    abstract visitUnary(expr: UnaryOperationTree): string; 
+    abstract visitStatlist(expr: StatListTree): string; 
+    abstract visitWhile(expr: WhileTree): string; 
+    abstract visitRepeat(expr: RepeatUntilTree): string; 
+    abstract visitIf(expr: IfTree): string; 
+    abstract visitFor(expr: ForTree): string; 
+    abstract visitIterator(expr: IteratorTree): string; 
+    abstract visitRange(expr: RangeTree): string; 
+    abstract visitFunction(expr: FunctionTree): string; 
+    abstract visitFunctionCall(expr: FunctionCallTree): string; 
+    abstract visitArray(expr: ArrayTree): string; 
+    abstract visitFullId(expr: FullIdTree): string; 
+    abstract visitIndex(expr: IndexAccessorTree): string; 
+    abstract visitDotName(expr: DotAccessorTree): string; 
+    abstract visitObject(expr: ObjectTree): string; 
+    abstract visitKeyValue(expr: KeyValueTree): string; 
+    abstract visitReturn(expr: ReturnTree): string; 
+    abstract visitBreak(expr: BreakTree): string; 
+    abstract visitContinue(expr: ContinueTree): string; 
+    
+    protected lowerPrecedence(parent: number, child: number): boolean {
+        const accessors = [PseudoLexer.LBRACK, PseudoLexer.DOT]
+        const multiplicative = [PseudoLexer.STAR, PseudoLexer.SLASH, PseudoLexer.DIV, PseudoLexer.MOD];
+        const additive = [PseudoLexer.PLUS, PseudoLexer.MINUS];
+        const comparison = [PseudoLexer.GREATERTHAN, PseudoLexer.GREATEREQUAL, PseudoLexer.LESSTHAN, PseudoLexer.LESSEQUAL, PseudoLexer.EQUALS, PseudoLexer.NOTEQUAL];
+        const and = [PseudoLexer.AND];
+        const or = [PseudoLexer.OR];
+
+        const precedence = [accessors, multiplicative, additive, comparison, and, or];
+
+        const parentPrecedence = precedence.map(ops => ops.includes(parent)).indexOf(true);
+        const childPrecedence = precedence.map(ops => ops.includes(child)).indexOf(true);
+
+        return parentPrecedence < childPrecedence;
+    }
+
+    protected getTree(tree: ExprTree, operator: number): string {
+        if (tree instanceof BinaryOperationTree && !(tree.operator.type in [PseudoLexer.LBRACK,  PseudoLexer.DOT])) {
+            const result = tree.accept(this)
+            if (this.lowerPrecedence(operator, tree.operator.type)) {
+                return `(${result})`
+            } else {
+                return result
+            }
+        } else {
+            return tree.accept(this)
+        }
+    }
+}
--- apps/frontend/src/lib/typstVisitor.ts
+++ apps/frontend/src/lib/typstVisitor.ts
@@ -1,13 +1,15 @@
 import { BinaryOperationTree, ExprTree, type ArrayTree, type AssignTree, type BreakTree, type ContinueTree, type DotAccessorTree, type ForTree, type FullIdTree, type FunctionCallTree, type FunctionTree, type IfTree, type IndexAccessorTree, type IteratorTree, type KeyValueTree, type ObjectTree, type ProgramTree, type RangeTree, type RepeatUntilTree, type ReturnTree, type StatListTree, UnaryOperationTree, type Visitor, type WhileTree } from "@interactive-pseudo/interpreter";
 import { PseudoLexer } from '@interactive-pseudo/parser'
+import { MarkupGenerationVisitor } from "./markupGenerationVisitor.js";
 
-export class TypstVisitor implements Visitor<string> {
+export class TypstVisitor extends MarkupGenerationVisitor {
 
     functions: Set<string>
     tabs: number
     tab: string
 
-    constructor(tab: string) {
+    constructor(tab: string, headers: boolean) {
+        super(headers)
         this.functions = new Set();
         this.tabs = 0
         this.tab = tab
@@ -18,9 +20,12 @@ export class TypstVisitor implements Visitor<string> {
     }
 
     visitProgram(program: ProgramTree): string {
-        let result = `#import "@preview/algorithmic:1.0.7"\n`
-        result += `#import algorithmic: style-algorithm, algorithm-figure\n`
-        result += `#show: style-algorithm\n`
+        let result = ''
+        if (this.headers) {
+            result += `#import "@preview/algorithmic:1.0.7"\n`
+            result += `#import algorithmic: style-algorithm, algorithm-figure\n`
+            result += `#show: style-algorithm\n`
+        }
         result += `#algorithm-figure(`
         this.tabs++;
         result += this.newlineTabs() + '"Algorithm Name",'
@@ -28,8 +33,8 @@ export class TypstVisitor implements Visitor<string> {
         result += this.newlineTabs() + `{`
         this.tabs++;
         result += this.newlineTabs() + `import algorithmic: *`
-        const stats = program.children.map(stat => stat.accept(this)).join(this.newlineTabs())
-        const functions = this.functions.entries().map(([_, value]) => `let ${value} = Fn.with("${value}")`).toArray().join(this.newlineTabs())
+        const stats = program.children.map(stat => stat instanceof ExprTree ? `Line(${stat.accept(this)})` : stat.accept(this)).join(this.newlineTabs())
+        const functions = this.functions.entries().map(([_, value]) => `let ${value} = Fn.with("${value.replaceAll('"', '\\"')}")`).toArray().join(this.newlineTabs())
         result += `${this.newlineTabs()}${functions}${this.newlineTabs()}${stats}`
         this.tabs--;
         result += this.newlineTabs() + `}`
@@ -38,7 +43,7 @@ export class TypstVisitor implements Visitor<string> {
         return result
     }
     visitAssign(assign: AssignTree): string {
-        return `Assign("${assign.id.accept(this)}", [$${assign.expr.accept(this)}$])`
+        return `Assign($${assign.id.accept(this)}$, [$${assign.expr.accept(this)}$])`
     }
     visitExpr(expr: ExprTree): string {
         if (expr instanceof BinaryOperationTree) {
@@ -48,7 +53,42 @@ export class TypstVisitor implements Visitor<string> {
         }
         throw new Error("Unexpected Tree type")
     }
+
+
+    getOperator(operator: number): string {
+        switch (operator) {
+            case PseudoLexer.LESSTHAN:
+                return '<'
+            case PseudoLexer.LESSEQUAL:
+                return '<='
+            case PseudoLexer.GREATERTHAN:
+                return '>'
+            case PseudoLexer.GREATEREQUAL:
+                return '>='
+            case PseudoLexer.EQUALS:
+                return '='
+            case PseudoLexer.NOTEQUAL:
+                return '!='
+            case PseudoLexer.PLUS:
+                return '+'
+            case PseudoLexer.MINUS:
+                return '-'
+            case PseudoLexer.STAR:
+                return 'dot'
+            case PseudoLexer.MOD:
+                return 'mod'
+            case PseudoLexer.AND:
+                return 'and'
+            case PseudoLexer.OR:
+                return 'or'
+            default:
+                return '??'
+        }
+    }
+
     visitBinary(expr: BinaryOperationTree): string {
+        const left = this.getTree(expr.left, expr.operator.type);
+        const right = this.getTree(expr.right, expr.operator.type);
         switch (expr.operator.type) {
             case PseudoLexer.LBRACK:
                 return `${expr.left.accept(this)}[${expr.right.accept(this)}]`
@@ -59,29 +99,32 @@ export class TypstVisitor implements Visitor<string> {
             case PseudoLexer.DIV:
                 return `floor(${expr.left.accept(this)} / ${expr.right.accept(this)})`
             default:
-                return `${expr.left.accept(this)} ${expr.operator.text} ${expr.right.accept(this)}`
+                return `${left} ${this.getOperator(expr.operator.type)} ${right}`
         }
     }
+
     visitUnary(expr: UnaryOperationTree): string {
         let operand = expr.operand instanceof ExprTree 
             ? expr.operand.accept(this) 
-            : expr.operand.type == PseudoLexer.INT || expr.operand.type == PseudoLexer.FLOAT
+            : expr.operand.type == PseudoLexer.INT || expr.operand.type == PseudoLexer.FLOAT || expr.operand.text.length <= 1
             ? expr.operand.text 
-            : `text("${expr.operand.text}")`
+            : `text("${expr.operand.text.replaceAll('"', '\\"')}")`
         if (expr.operator) {
             return `${expr.operator.text} ${operand}`
         } else {
             return `${operand}`
         }
     }
+
     visitStatlist(expr: StatListTree): string {
         this.tabs++;
-        let stats = expr.stats.map(stat => stat.accept(this))
+        let stats = expr.stats.map(stat => stat instanceof ExprTree ? `Line(${stat.accept(this)})` : stat.accept(this))
         let result = `{${this.newlineTabs()}${stats.join(this.newlineTabs())}`
         this.tabs--;
         result += `${this.newlineTabs()}}`
         return result
     }
+
     visitWhile(expr: WhileTree): string {
         let result = `While(`;
         this.tabs++;
@@ -90,9 +133,11 @@ export class TypstVisitor implements Visitor<string> {
         result += `${this.newlineTabs()})`;
         return result;
     }
+
     visitRepeat(expr: RepeatUntilTree): string {
         throw new Error("Algorithmic does not support repeat until");
     }
+
     visitIf(expr: IfTree): string {
         this.tabs++;
         const conditions = expr.conditions.map(cond => cond.accept(this))
@@ -109,6 +154,7 @@ export class TypstVisitor implements Visitor<string> {
         result += this.newlineTabs() + ')'
         return result
     }
+
     visitFor(expr: ForTree): string {
         let result = `For(`;
         this.tabs++;
@@ -117,18 +163,21 @@ export class TypstVisitor implements Visitor<string> {
         result += `${this.newlineTabs()})`;
         return result;
     }
+
     visitIterator(expr: IteratorTree): string {
         return `[${expr.id.text} $in ${expr.iterator.accept(this)}$]`
     }
+
     visitRange(expr: RangeTree): string {
         return `${expr.from.accept(this)}$ ... $${expr.to.accept(this)} `
     }
+
     visitFunction(expr: FunctionTree): string {
         let result = `Function(`;
         this.tabs++;
-        const name = `"${expr.name.text}"`
+        const name = `"${expr.name.text.replaceAll('"', '\\"')}"`
         result += this.newlineTabs() + `${name},`
-        const args = expr.args.map(arg => arg.text.length > 1 ? `text("${arg.text}")` : `$${arg.text}$`).join(", ")
+        const args = expr.args.map(arg => arg.text.length > 1 ? `text("${arg.text.replaceAll('"', '\\"')}")` : `$${arg.text}$`).join(", ")
         result += this.newlineTabs() + `(${args}),`
         const body = expr.stats.accept(this)
         result += body
@@ -136,41 +185,49 @@ export class TypstVisitor implements Visitor<string> {
         result += `${this.newlineTabs()})`
         return result
     }
+
     visitFunctionCall(expr: FunctionCallTree): string {
         this.functions.add(expr.name.text)
         const args = expr.args.map(arg => `#[$${arg.accept(this)}$]`).join(", ")
         return ` $#${expr.name.text}[${args}].join()$ `
     }
+
     visitArray(expr: ArrayTree): string {
         const values = expr.elements.map(element => `#[$${element.accept(this)}$]`).join(", ")
         return `[${values}]`
     }
+
     visitFullId(expr: FullIdTree): string {
-        const name = expr.name.text
+        const name = `text("${expr.name.text}")`
         const accessors = expr.accessors.map(accessor => accessor.accept(this)).join("")
         return `${name}${accessors}`
     }
+
     visitIndex(expr: IndexAccessorTree): string {
-        return `[$${expr.index.accept(this)}$]`
+        return `[${expr.index.accept(this)}]`
     }
+
     visitDotName(expr: DotAccessorTree): string {
         return `.${expr.name.text}`
     }
+
     visitObject(expr: ObjectTree): string {
         const elements = expr.elements.map(element => element.accept(this)).join(",")
         return `{${elements}}`
     }
     visitKeyValue(expr: KeyValueTree): string {
-        return `${expr.key.text}: $${expr.value.accept(this)}$`
+        return `text("${expr.key.text.replaceAll('"', '\\"')}"): ${expr.value.accept(this)}`
     }
+
     visitReturn(expr: ReturnTree): string {
         return `Return[$${expr.value.accept(this)}$]`
     }
+
     visitBreak(expr: BreakTree): string {
-        return `Break()`
-    }
-    visitContinue(expr: ContinueTree): string {
-        return `Continue()`
+        return `Break`
     }
 
+    visitContinue(expr: ContinueTree): string {
+        return `(smallcaps[continue],)`
+    }
 }
