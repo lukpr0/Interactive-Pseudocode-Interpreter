df3033547a6c81c86310ccd32649e9cc5bea348f
Lukas <lukas.hu.02@gmail.com>
2025-12-29 20:44:42 +0100

    added better errors for unary expressions

--- packages/interpreter/src/Interpreter/Errors/index.ts
+++ packages/interpreter/src/Interpreter/Errors/index.ts
@@ -1,4 +1,4 @@
 export { default as EmptyStackError } from './EmptyStackError.js'
 export * from './TypeError.js'
-export { default as VariableError } from './VariableError.js'
+export { VariableError } from './VariableError.js'
 export { default as FeatureNotImplementedError } from './FeatureNotImplementedError.js'
