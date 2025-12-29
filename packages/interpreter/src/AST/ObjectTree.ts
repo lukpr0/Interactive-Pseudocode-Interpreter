e9501ef842c757b5d525d3ed85759a8a211c5862
Lukas <lukas.hu.02@gmail.com>
2025-12-29 22:54:59 +0100

    added better errors for object

--- packages/interpreter/src/AST/ObjectTree.ts
+++ packages/interpreter/src/AST/ObjectTree.ts
@@ -2,14 +2,18 @@ import type Tree from "./Tree.js";
 import type Visitor from "./Visitor.js";
 import { ExprTree } from "./ExprTree.js";
 import type KeyValueTree from "./KeyValueTree.js";
+import type InfoTree from "./InfoTree.js";
+import type { Token } from "antlr4";
 
-export default class ObjectTree extends ExprTree implements Tree {
+export default class ObjectTree extends ExprTree implements Tree, InfoTree {
 
     elements: KeyValueTree[]
+    infoToken: Token;
 
-    constructor(elements: KeyValueTree[]) {
+    constructor(elements: KeyValueTree[], token: Token) {
         super();
         this.elements = elements;
+        this.infoToken = token;
     }
 
     accept<T>(visitor: Visitor<T>): T {
