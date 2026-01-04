aed98e28071caa754f65734b6d02499ebe958a65
Lukas <lukas.hu.02@gmail.com>
2026-01-04 22:27:40 +0100

    error on jump statements on unallowed context

--- packages/interpreter/src/AST/BreakTree.ts
+++ packages/interpreter/src/AST/BreakTree.ts
@@ -1,7 +1,20 @@
+import type { Token } from "antlr4";
+import type InfoTree from "./InfoTree.js";
+import type NodeLocation from "./NodeLocations.js";
 import type Tree from "./Tree.js";
 import type Visitor from "./Visitor.js";
+import { tokenToNodeLocation } from "./NodeLocations.js";
 
-export default class BreakTree implements Tree {
+export default class BreakTree implements Tree, InfoTree {
+
+    token: Token;
+    location: NodeLocation;
+
+    constructor(token: Token) {
+        this.token = token;
+        this.location = tokenToNodeLocation(token);
+    }
+
     accept<T>(visitor: Visitor<T>): T {
         return visitor.visitBreak(this);
     }
--- packages/interpreter/src/AST/ContinueTree.ts
+++ packages/interpreter/src/AST/ContinueTree.ts
@@ -1,7 +1,20 @@
+import type { Token } from "antlr4";
 import type Tree from "./Tree.js";
 import type Visitor from "./Visitor.js";
+import type InfoTree from "./InfoTree.js";
+import type NodeLocation from "./NodeLocations.js";
+import { tokenToNodeLocation } from "./NodeLocations.js";
 
-export default class ContinueTree implements Tree {
+export default class ContinueTree implements Tree, InfoTree {
+
+    token: Token;
+    location: NodeLocation;
+
+    constructor(token: Token) {
+        this.token = token;
+        this.location = tokenToNodeLocation(token);
+    }
+
     accept<T>(visitor: Visitor<T>): T {
         return visitor.visitContinue(this);
     }
