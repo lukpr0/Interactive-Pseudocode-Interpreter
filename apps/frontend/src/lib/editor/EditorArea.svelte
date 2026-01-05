c7a9254f5109ce1a42c443d857a9984fff88e24b
Lukas <lukas.hu.02@gmail.com>
2026-01-05 21:10:34 +0100

    refactor ui

---
+++ apps/frontend/src/lib/editor/EditorArea.svelte
@@ -0,0 +1,21 @@
+<div class="code-wrapper">
+    <Codemirror bind:value={shared.code} vimMode={shared.vimMode} errors={shared.errorLocations} onchange={changeCode}></Codemirror>
+</div>
+
+<script lang="ts">
+    import Codemirror from "./Codemirror.svelte";
+    import { shared } from "$lib/shared/state.svelte";
+
+    let { changeCode } = $props();
+
+</script>
+
+<style>
+    .code-wrapper {
+        margin: 1em;
+        height: 60vh;
+        grid-column: span 8;
+        grid-row: span 6;
+        overflow: auto;
+    }
+</style>
