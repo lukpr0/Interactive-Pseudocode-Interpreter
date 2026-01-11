a86cd3160ec502d5cd212aff9020af5ea913edf4
lukpr0 <lukas.1011@web.de>
2026-01-11 20:02:23 +0100

    added vitest

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,16 @@
+/// <reference types="vitest/config" />
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
 export default defineConfig({
 
 	build: {
 		assetsInlineLimit: Infinity
 	},
 
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	test: {
+		include: ["./test/**/*.{test,spec}.{js,ts}"],
+        exclude: ['**/node_modules', '**/dist'],
+	}
 });
